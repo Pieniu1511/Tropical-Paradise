92c1c3d43eb526cf1002f77f3fb5696fb598f684
Edmund <pieniu1511@gmail.com>
2022-06-04 20:24:10 +0200

    corrected animation

--- js/main.js
+++ js/main.js
@@ -1,27 +1,37 @@
-const nav = document.querySelector('.nav');
-const navBtn = document.querySelector('.burger-btn');
-const allNavItems = document.querySelectorAll('.nav__item');
+const nav = document.querySelector('.nav')
+const navBtn = document.querySelector('.burger-btn')
+const allNavItems = document.querySelectorAll('.nav__item')
 
 const handleNav = () => {
-    nav.classList.toggle('nav--active')
+	nav.classList.toggle('nav--active')
 
-    allNavItems.forEach(item => {
-        item.addEventListener('click', () => {
-            nav.classList.remove('nav--active')
-        })
-    })
+	allNavItems.forEach(item => {
+		item.addEventListener('click', () => {
+			nav.classList.remove('nav--active')
+		})
+	})
 
-    handleNavItemsAnimation();
+	handleNavItemsAnimation()
 }
 
 const handleNavItemsAnimation = () => {
-    let delayTime = 0;
+	let delayTime = 0
 
+	allNavItems.forEach(item => {
+		item.classList.toggle('nav-items-animation')
+		item.style.animationDelay = '.' + delayTime + 's'
+		delayTime++
+	})
+}
+
+const deleteAnimation = () => {
     allNavItems.forEach(item => {
-        item.classList.toggle('nav-items-animation')
-        item.style.animationDelay = '.' + delayTime + "s";
-        delayTime++;
+        item.classList.remove('nav-items-animation')
     })
 }
+
+Array.from(allNavItems).forEach((link) => {
+    link.addEventListener("click", deleteAnimation);
+  });
 
 navBtn.addEventListener('click', handleNav)
